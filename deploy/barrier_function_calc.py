b8bcdbe81540940f2bc207b6deb2e8951657c207
kaseng <ks6295@nyu.edu>
2026-03-08 00:35:14 -0500

    Plotted tuning parameters

--- deploy/barrier_function_calc.py
+++ deploy/barrier_function_calc.py
@@ -10,14 +10,26 @@ class controlBarrierFunction():
 
     def __init__(
         self,
-        model,
-        data,
-        xml_path
+        xml_path,
+        exp_config
     ):
-        self.model = model
-        self.data = data
 
-        # extract mesh information from xml
+        # experiment configuration parameters
+        target_body_origin = exp_config["target_pos"]
+        shelf_pos = exp_config["shelf_pos"]
+        moving_obs_pos = exp_config["moving_obs_pos"]
+        self.moving_obs_cmd = np.array(exp_config["moving_obs_cmd"])
+        self.alpha_static = exp_config["alpha_static"]
+        self.alpha_moving = exp_config["alpha_moving"]
+        self.gamma = exp_config["gamma"]
+        self.x_weight = exp_config["x_weight"]
+        self.y_weight = exp_config["y_weight"]
+        self.theta_weight = exp_config["theta_weight"]
+        self.slack_static = exp_config["slack_static_weight"]
+        self.slack_moving = exp_config["slack_moving_weight"]
+        self.slack_workspace = exp_config["slack_workspace_weight"]
+
+        # xml modifications and data extraction
         tree = ET.parse(xml_path)
         root = tree.getroot()
         target_mesh = root.find(".//mesh[@name='target_mesh']")
@@ -25,9 +37,18 @@ class controlBarrierFunction():
         target_mesh_path = "./" + target_mesh_path[target_mesh_path.find("custom_meshes"):]
         self.target_mesh = o3d.io.read_triangle_mesh(target_mesh_path)
         self.target_points = np.unique(np.asarray(self.target_mesh.vertices), axis=0)
+        # update target position
         target_body = root.find(".//body[@name='target']")
-        target_body_origin = target_body.get("pos")
-        target_body_origin = np.fromstring(target_body_origin, sep=' ')
+        target_body.set("pos", target_body_origin)
+        # update shelf position
+        shelf_body = root.find(".//geom[@name='obs4']")
+        shelf_body.set("pos", shelf_pos)
+        tree.write(xml_path)
+        print("XML Modified")
+
+        self.model = mujoco.MjModel.from_xml_path(xml_path)
+        self.data = mujoco.MjData(self.model)
+        self.data.qpos[19:22] = moving_obs_pos  
 
         # represent target as a bounding box and express center in world coordinates
         min_x = np.min(self.target_points[:,0])
@@ -38,7 +59,7 @@ class controlBarrierFunction():
         max_z = np.max(self.target_points[:,2])
         self.target_pos = np.array([(max_x + min_x)/2, 
                                        (max_y + min_y)/2 ,
-                                       (max_z + min_z)/2]) + target_body_origin
+                                       (max_z + min_z)/2]) + np.fromstring(target_body_origin, dtype=np.float64, sep=' ')
         
         # convert workspace into convex hull for target reachability awareness
         pointcloud_path = "./pointcloud/workspace_point_cloud_filtered.npy"
@@ -121,7 +142,7 @@ class controlBarrierFunction():
 
         return self.h_moving_obs, self.grad_h_moving_obs, dh_dt
 
-    def workspace_calc(self, alpha, beta, gamma, theta):
+    def workspace_calc(self, theta):
         rel_target_pos = self.target_pos - self.data.qpos[:3]
         A = self.workspace_A.copy()
         b = self.workspace_b.copy()
@@ -138,11 +159,6 @@ class controlBarrierFunction():
             ])
         rotated_target = R_world_to_body @ rel_target_pos
         self.workspace_target_distance = np.linalg.norm(rotated_target - self.workspace_center)
-        # sigmoid =  1/(1+np.exp(beta*(self.workspace_target_distance - gamma)))
-        # self.h_workspace = alpha * sigmoid
-        # print("Rotated Target:", rotated_target)
-        # print("Workspace Center:", self.workspace_center)
-        # print("Vector from center to target:", rotated_target - self.workspace_center)
         self.h_workspace = self.workspace_target_distance
         
         grad_d_wrt_pos = -(rotated_target - self.workspace_center)/self.workspace_target_distance
@@ -157,54 +173,23 @@ class controlBarrierFunction():
 
         return self.h_workspace, self.grad_h_workspace
     
-    def composite_calc(self, theta, mode):
-        '''mode 0 for static calculations, else for moving obstacle'''
-        if mode:
-            h_obs, grad_h_obs = self.static_obs_calc(theta)
-            dh_dt = 0
-            h_work, grad_h_work = self.workspace_calc(0.5, 2, 0.5, theta)
-
-            h_composite = h_obs + h_work
-            grad_h = grad_h_obs + grad_h_work
-
-            print("h static_obstacle:", h_obs)
-            print("h workspace:", h_work)
-            print("static obstacle grad:", grad_h_obs)
-            print("workspace grad:", grad_h_work)
-            print("h static comp:", h_composite)
-        else:
-            h_obs, grad_h_obs, dh_dt = self.moving_obs_calc(theta)
-            h_work, grad_h_work = self.workspace_calc(0.5, 2, 0.5, theta)
-
-            h_composite = h_obs + h_work
-            grad_h = grad_h_obs + grad_h_work
-
-            print("h_moving_obstacle:", h_obs)
-            print("moving obstacle grad:", grad_h_obs)
-            print("h moving comp:", h_composite)
-
-        return h_composite, grad_h, dh_dt
-    
     def qp_filter(self, u_d, theta):
         '''u_d is the policy output command (3,)'''
 
-        alpha_1 = 0.2
-        alpha_2 = 0.2
-        alpha_3 = 0.2
         h_static, grad_h_static = self.static_obs_calc(theta)
         h_moving, grad_h_moving, dh_dt = self.moving_obs_calc(theta)
-        h_workspace, grad_h_workspace = self.workspace_calc(0.5, 2, 0.5, theta)
+        h_workspace, grad_h_workspace = self.workspace_calc(theta)
         grad_h_static = np.concatenate((grad_h_static, [1], [0], [0]))
         grad_h_moving = np.concatenate((grad_h_moving, [0], [1], [0]))
         grad_h_workspace = np.concatenate((grad_h_workspace, [0], [0], [-1]))
         
         # QP solver parameters
-        P = np.diag([1.0, 1.0, 0.1, 1000.0, 1000.0, 200.0])
+        P = np.diag([self.x_weight, self.y_weight, self.theta_weight, self.slack_static, self.slack_moving, self.slack_workspace])
         q = -P @ np.concatenate((u_d, [0.0], [0.0], [0.0]))
         G = np.vstack((-grad_h_static, -grad_h_moving, grad_h_workspace))
-        h = np.array([[alpha_1 * h_static],
-                      [alpha_2 * h_moving + dh_dt],
-                      [-alpha_3 * h_workspace]])
+        h = np.array([[self.alpha_static * h_static],
+                      [self.alpha_moving * h_moving + dh_dt],
+                      [-self.gamma * h_workspace]])
         lb = 1.0 * np.array([-1,-1,-1, 0, 0, 0])
         ub = 1.0 * np.array([1, 1, 1, 10, 10, 10])
         # print("Gu <", h)
