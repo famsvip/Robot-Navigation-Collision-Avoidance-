be71dd7902ca30c266c5d594af41d031fd0c8ff7
kaseng <ks6295@nyu.edu>
2026-03-30 22:24:00 -0400

    Scenario 3 run

--- deploy/deploy_mujoco_controller.py
+++ deploy/deploy_mujoco_controller.py
@@ -167,7 +167,7 @@ if __name__ == "__main__":
     mod_cmds = []
     obs_frc = []
     constraint_values = []
-    max_recorded_step = 1e4
+    max_recorded_step = 2500
 
     try:
         with mujoco.viewer.launch_passive(m, d) as viewer:
@@ -196,7 +196,7 @@ if __name__ == "__main__":
                     mod_cmds.append(modified_cmd)
                     print(d.qvel[:2])
                     terminal_vel = np.linalg.norm(d.qvel[:2])
-                    constraint_values.append([cbf.h_comp_static, cbf.h_static_obs, cbf.h_workspace, np.linalg.norm(cbf.grad_h_static_obs), np.linalg.norm(cbf.grad_h_workspace), cbf.static_slack, terminal_vel])
+                    constraint_values.append([cbf.h_static_obs, cbf.h_static_obs, cbf.h_moving_obs, np.linalg.norm(cbf.grad_h_static_obs), moving_slack, static_slack, terminal_vel])
                     contact_check(m, d, obs_frc)
                     print("Recording")
 
